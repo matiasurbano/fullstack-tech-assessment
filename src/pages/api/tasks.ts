fa4c5ff779d0a25e49ec0fd303d154ab599b79dc
Matias Urbano <matias.urbano@gmail.com>
2022-11-27 05:23:44 +1100

    Filtering task my title

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -1,14 +1,21 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
 //
 // This API route can be called as GET /api/tasks
 // It returns the list of all tasks in the app
 // It takes an optional query param 'filter', which can be used to filter
-// out tasks based on their title (TODO!)
+// out tasks based on their title
 //
-async function getTasks(includeTasks: boolean = false) {
-    return await prisma.task.findMany()
+async function getTasks(filter: string | string[] | undefined) {
+    const filterCondition = filter ? {
+        where: {
+            title: {
+                contains: Array.isArray(filter) ? filter[0] : filter 
+            }
+        }
+    } : undefined
+    return await prisma.task.findMany(filterCondition)
 }
 
 export default async function handler(
@@ -20,9 +27,8 @@ export default async function handler(
             res.status(405).end();
             return;
         }
-
-        const {filter} = req.query;
-        const tasks = await getTasks()
+        const filter = req.query?.filter
+        const tasks = await getTasks(filter)
 
         res.status(200).json({
             items: tasks,
